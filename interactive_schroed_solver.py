7a167a38c526f2b4348afc47a7850587379f3ec1
Michael Laplante <laplante.michael@hotmail.com>
2022-02-17 18:54:18 -0500

    Added docstrings to the interactive example

--- interactive_schroed_solver.py
+++ interactive_schroed_solver.py
@@ -4,20 +4,20 @@ from matplotlib import pyplot as plt
 #   Physical Constants:
 h_bar = 1.05457e-34
 MeV = 1.60217648e-13
-omega = 5.34e21                 #Harmonic oscillator frequency
+omega = 5.34e21                 # Harmonic oscillator frequency
 energy_scale = 3.5
 
 #   Calculation precision:
-eps = 1.0e-11                   #Required precision for the solution to achieve
-n_divisions = 1200              #Number of divisions of the integration region
-n_energyLev = 5                 #Number of energy levels to compute
-n_zeros = 0                     #Number of zeroes in the solution
-x_range = 11.0                  #Integration region length
-x_delta = x_range / n_divisions #Width of each division (spatial resolution)
+eps = 1.0e-11                   # Required precision for the solution to achieve
+n_divisions = 1200              # Number of divisions of the integration region
+n_energyLev = 5                 # Number of energy levels to compute
+n_zeros = 0                     # Number of zeroes in the solution
+x_range = 11.0                  # Integration region length
+x_delta = x_range / n_divisions # Width of each division (spatial resolution)
 
 #   Define the potential:
 def E_pot_fun(x):
-    return 0.5 * pow(x,2.0)       #Harmonic potential
+    return 0.5 * pow(x,2.0)     # Harmonic potential
 
 region = np.arange(-x_range/2,x_range/2,x_range/n_divisions)
 E_pot = (np.vectorize(E_pot_fun))(region)
@@ -38,7 +38,7 @@ E_upperBound = 10.0
 E_trial = E_upperBound
 E_EigenEnergies = np.zeros(n_energyLev)
 
-#   Plotting prep
+#   Plotting preparations
 plt.ion()
 fig = plt.figure()
 a1 = fig.add_axes([0,0,1,1])
@@ -60,6 +60,9 @@ def draw_plot(index):
     plt.pause(0.005)
     plt.clf()
 
+#############
+#   Solver  #
+#############
 
 def k_squared(i,E):
     return 2.0 * (E - E_pot[i])         #k^2(x) = 2m(E - V(x)) / hbar^2
@@ -71,8 +74,15 @@ def numerov_approx(psi_m1,psi_m2,i,E):
     return ((2.0 * term1 * psi_m1) - term2 * psi_m2) / denom
 
 def coarse_search_bounds(ith_energyLev):
-    #   Iterative step in search of an energy upper bound. Return True if the
-    #   estimated bounds achieve the correct amount of zeros in the solution.
+    """
+    Iterative step in search of an energy upper bound.
+
+    Parameters:
+    ith_energyLev (int): the energy level of the solution
+
+    Returns:
+    bool: True if the bounds have the appropriate amount of zeros, false otherwise. 
+    """
     global E_upperBound
     global E_lowerBound
     global n_zeros
@@ -101,8 +111,15 @@ def coarse_search_bounds(ith_energyLev):
         return True
 
 def refine_bounds(ith_energyLev):
-    #   Refines the energy level by looking at the right boundary condition.
-    #   Returns True if the precision goal is achieved, false otherwise.
+    """
+    Refines the energy level by looking at the right boundary condition.
+    
+    Parameters:
+    ith_energyLev (int): the energy level of the searched solution.
+
+    Returns: 
+    bool: True if the precision goal is achieved, false otherwise.
+    """
     global E_upperBound
     global E_lowerBound
     global n_zeros
@@ -123,7 +140,10 @@ def refine_bounds(ith_energyLev):
     return (E_upperBound - E_lowerBound) < eps
 
 
-#   Main loop
+################
+#   Main Loop  #
+################
+
 for ith_energyLev in range(1,n_energyLev) :
 
     print("Energy level - {}: searching solution.".format(ith_energyLev))
@@ -152,8 +172,6 @@ for ith_energyLev in range(1,n_energyLev) :
     #   Add units to energy:
     E_trial = (E_lowerBound + E_upperBound) / 2
     E_EigenEnergies[ith_energyLev] = energy_scale * E_trial
-
-    #   Plot solution:
 
     #   Reset loop:
     E_upperBound = E_trial
